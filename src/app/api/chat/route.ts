b46ac136505a912409f769fc608dca5434ca1068
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-04-28 07:28:27 -0300

    feat: added a new method to ignore acentos

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -2,7 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 // Respostas específicas para palavras-chave
 const replies = {
-  nextGame: "O próximo jogo da FURIA é dia 28/04 contra a Team Liquid às 18h!",
+  nextGame:
+    "VALORANT: 18 de abril de 2025 às 21h: FURIA vs. MIBR, League of Legends (LoL): 03 de maio de 2025 às 13h: FURIA vs. RED Canids , 11 de maio de 2025 às 10h: FURIA vs. Fluxo W7M",
   players:
     "O time atual da FURIA CS:GO é: KSCERATO, yuurih, arT, chelo, FalleN. \n O time atual da FURIA de VALORANT é: f4stzz, mwzera, frz, Khalil, aspas. \n O time atual da FURIA de League of Legends é: Route, esA, Robo, Mari, DyNquedo.",
   lastResult: "FURIA venceu a MIBR por 2-1 no último jogo!",
@@ -12,10 +13,13 @@ const replies = {
 
 // Função de correspondência de mensagem
 const matchMessage = (message: string) => {
-  const normalizedMessage = message.toLowerCase();
+  const normalizedMessage = message
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
 
   // Respostas específicas para palavras-chave
-  if (normalizedMessage.includes("próximo jogo")) {
+  if (normalizedMessage.includes("proximos jogos")) {
     return replies.nextGame;
   }
 
